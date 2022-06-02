0b3dd77770128b08a8e8ef7d051a62336eaa451e
cv <34649812+cpvalente@users.noreply.github.com>
2022-06-02 19:08:30 +0200

    feat/55: event status icons

--- client/src/features/editors/EventBlockNew/EventBlockNew.jsx
+++ client/src/features/editors/EventBlockNew/EventBlockNew.jsx
@@ -7,11 +7,19 @@ import { IconButton } from '@chakra-ui/button';
 import { IoRemoveCircleSharp } from '@react-icons/all-files/io5/IoRemoveCircleSharp';
 import { IoAdd } from '@react-icons/all-files/io5/IoAdd';
 import { IoSettingsSharp } from '@react-icons/all-files/io5/IoSettingsSharp';
+import { IoArrowForwardCircle } from '@react-icons/all-files/io5/IoArrowForwardCircle';
+import { IoLink } from '@react-icons/all-files/io5/IoLink';
 import { IoPlay } from '@react-icons/all-files/io5/IoPlay';
+import { IoFlask } from '@react-icons/all-files/io5/IoFlask';
+import { IoHourglassOutline } from '@react-icons/all-files/io5/IoHourglassOutline';
+import { IoReturnDownForward } from '@react-icons/all-files/io5/IoReturnDownForward';
+import { IoTimerOutline } from '@react-icons/all-files/io5/IoTimerOutline';
+import { IoRocketSharp } from '@react-icons/all-files/io5/IoRocketSharp';
 import { IoReload } from '@react-icons/all-files/io5/IoReload';
 import { IoReorderTwo } from '@react-icons/all-files/io5/IoReorderTwo';
 import { Editable, EditableInput, EditablePreview } from '@chakra-ui/editable';
 import { getAccessibleColour } from '../../../app/utils/styleUtils';
+import { Tooltip } from '@chakra-ui/tooltip';
 
 const blockBtnProps = {
   size: 'sm',
@@ -23,9 +31,12 @@ const blockBtnProps = {
 
 function selectPlaybackStyle(playback) {
   switch (playback) {
-    case 'play': return style.play;
-    case 'pause': return style.pause;
-    default: return ''
+    case 'play':
+      return style.play;
+    case 'pause':
+      return style.pause;
+    default:
+      return '';
   }
 }
 
@@ -47,12 +58,15 @@ export default function EventBlockNew(props) {
 
   const binderColours = getAccessibleColour(colour);
   const progress = 0.2;
-  const progressStyle = selectPlaybackStyle(state)
+  const progressStyle = selectPlaybackStyle(state);
 
   return (
     <div className={style.eventBlock}>
       <div className={`${style.progressBg} ${progressStyle}`}>
-        <div className={`${style.progressBar} ${progressStyle}`} style={{ width: `${progress * 100}%` }} />
+        <div
+          className={`${style.progressBar} ${progressStyle}`}
+          style={{ width: `${progress * 100}%` }}
+        />
       </div>
       <div className={style.binder} style={{ ...binderColours }}>
         <IoReorderTwo className={style.drag} />
@@ -78,9 +92,15 @@ export default function EventBlockNew(props) {
         <IconButton icon={<IoSettingsSharp />} aria-label='event options' {...blockBtnProps} />
       </div>
       <div className={style.eventStatus}>
-        <IoAddCircleOutline fontSize='24px' />
-        <IoAddCircleOutline fontSize='24px' />
-        <IoAddCircleOutline fontSize='24px' />
+        <Tooltip openDelay={100} label='Next event' shouldWrapChildren='disabled'>
+          <IoReturnDownForward fontSize='18px' style={{color: "white", background: "#212121", borderRadius: "999px", padding: "2px"}} />
+        </Tooltip>
+        <Tooltip openDelay={100} label='Event has delay' shouldWrapChildren='disabled'>
+          <IoTimerOutline fontSize='18px' style={{color: "white", background: "#212121", borderRadius: "999px", padding: "2px"}} />
+        </Tooltip>
+        <Tooltip openDelay={100} label='Event has automations' shouldWrapChildren='disabled'>
+          <IoLink fontSize='18px' style={{color: "white", background: "#212121", borderRadius: "999px", padding: "2px"}} />
+        </Tooltip>
       </div>
     </div>
   );
