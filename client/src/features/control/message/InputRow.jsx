546ed2cdf7d956c3f3e34ea5111a32c70004368e
cv <34649812+cpvalente@users.noreply.github.com>
2022-06-06 22:30:45 +0200

    refactor styling

--- client/src/features/control/message/InputRow.jsx
+++ client/src/features/control/message/InputRow.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { IconButton } from '@chakra-ui/button';
 import { Editable, EditableInput, EditablePreview } from '@chakra-ui/editable';
 import { Tooltip } from '@chakra-ui/tooltip';
-import { IconButton } from '@chakra-ui/button';
 import { IoSunny } from '@react-icons/all-files/io5/IoSunny';
 import PropTypes from 'prop-types';
+
 import style from './MessageControl.module.scss';
 
 export default function InputRow(props) {
   const { label, placeholder, text, visible, actionHandler, changeHandler } = props;
 
   return (
-    <div className={visible && style.inputRowActive}>
+    <div className={`${visible ? style.inputRowActive: ''}`}>
       <span className={style.label}>{label}</span>
       <div className={style.inputItems}>
         <Editable
