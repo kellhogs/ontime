c517c3a7ddb7c7bb7a51297de9b7c59ecf069cbe
cv <34649812+cpvalente@users.noreply.github.com>
2022-07-01 22:40:01 +0200

    feat/55: create event editor

--- client/src/features/event-editor/EventEditor.jsx
+++ client/src/features/event-editor/EventEditor.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, IconButton } from '@chakra-ui/button';
 import { Input } from '@chakra-ui/react';
 
 import style from './EventEditor.module.scss';
@@ -7,20 +8,45 @@ export default function EventEditor() {
   return (
     <div className={style.eventEditorMenu}>
       <div>
-        <label>Title</label>
-        <Input />
+        <div className={style.inline}>
+          <label>Start Time</label>
+          <IconButton size='sm' />
+          <Input size='sm' />
+        </div>
+        <div className={style.inline}>
+          <label>End time</label>
+          <IconButton size='sm' />
+          <Input size='sm' />
+        </div>
+        <div className={style.inline}>
+          <label>Duration</label>
+          <IconButton size='sm' />
+          <Input size='sm' />
+        </div>
+        <div>
+          <Button>Is Public</Button>
+        </div>
       </div>
       <div>
-        <label>Subtitle</label>
-        <Input />
-      </div>
-      <div>
-        <label>Presenter</label>
-        <Input />
-      </div>
-      <div>
-        <label>Notes</label>
-        <Input />
+        <div className={style.inline}>
+          <label>Colour</label>
+          <Input size='sm' type='color' />
+          <label>Title</label>
+          <Input size='sm' />
+        </div>
+        <div className={style.inline}>
+          <label>Subtitle</label>
+          <Input size='sm' />
+        </div>
+        <div className={style.inline}>
+          <label>Presenter</label>
+          <Input size='sm' />
+        </div>
+        <div className={style.inline}>
+          <label>Notes</label>
+          <Input size='sm' />
+        </div>
+        <div>OSC</div>
       </div>
     </div>
   );
