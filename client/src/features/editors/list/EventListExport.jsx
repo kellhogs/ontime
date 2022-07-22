470686d474e6c54d1fb0b91d273ebcc59fc03def
cv <34649812+cpvalente@users.noreply.github.com>
2022-07-22 22:45:43 +0200

    refactor(EventBlock): remove unused collapse context

--- client/src/features/editors/list/EventListExport.jsx
+++ client/src/features/editors/list/EventListExport.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@chakra-ui/layout';
 import { FiArrowUpRight } from '@react-icons/all-files/fi/FiArrowUpRight';
 
-import { CollapseProvider } from '../../../app/context/CollapseContext';
 import { CursorProvider } from '../../../app/context/CursorContext';
 import ErrorBoundary from '../../../common/components/errorBoundary/ErrorBoundary';
 import { handleLinks } from '../../../common/utils/linkUtils';
@@ -14,18 +13,16 @@ import style from '../Editor.module.scss';
 export default function EventListExport() {
   return (
     <CursorProvider>
-      <CollapseProvider>
-        <Box className={style.editor}>
-          <h1>Event List</h1>
-          <FiArrowUpRight
-            className={style.corner}
-            onClick={(event) => handleLinks(event, 'eventlist')}
-          />
-          <ErrorBoundary>
-            <EventListWrapper />
-          </ErrorBoundary>
-        </Box>
-      </CollapseProvider>
+      <Box className={style.editor}>
+        <h1>Event List</h1>
+        <FiArrowUpRight
+          className={style.corner}
+          onClick={(event) => handleLinks(event, 'eventlist')}
+        />
+        <ErrorBoundary>
+          <EventListWrapper />
+        </ErrorBoundary>
+      </Box>
     </CursorProvider>
   );
 }
